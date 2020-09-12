47ccd4afedc33e44cc037962d1b7e253652a2ac2
vsamaru <vsamaru@gmail.com>
2020-09-12 05:06:23 +0300

    Update ao.js

--- pages/ao.js
+++ pages/ao.js
@@ -1,39 +1,58 @@
-import React from 'react';
-import Router from 'next/router';
-import { Text } from 'react-native-web';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-// https://github.com/zeit/next.js#imperatively
-export default Link extends React.Component {
-  static propTypes = Text.propTypes;
-  
-  blur() {
-    this._textRef.blur();
-  }
-  
-  focus() {
-    this._textRef.focus();
-  }
-  
-  setNativeProps(props) {
-    this._textRef.setNativeProps(props)
-  }
-  
-  _handlePress = () => {
-    Router.push(this.props.href);
+const App = () => {
+  const [timesPressed, setTimesPressed] = useState(0);
+
+  let textLog = '';
+  if (timesPressed > 1) {
+    textLog = timesPressed + 'x onPress';
+  } else if (timesPressed > 0) {
+    textLog = 'onPress';
   }
 
-  _setRef = (c) => {
-    this._textRef = c;
+  return (
+    <View>
+      <Pressable
+        onPress={() => {
+          setTimesPressed((current) => current + 1);
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? 'rgb(210, 230, 255)'
+              : 'white'
+          },
+          styles.wrapperCustom
+        ]}>
+        {({ pressed }) => (
+          <Text style={styles.text}>
+            {pressed ? 'Pressed!' : 'Press Me'}
+          </Text>
+        )}
+      </Pressable>
+      <View style={styles.logBox}>
+        <Text testID="pressable_press_console">{textLog}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6
+  },
+  logBox: {
+    padding: 20,
+    margin: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#f0f0f0',
+    backgroundColor: '#f9f9f9'
   }
-  
-  render() {
-    return (
-      <Text
-        accessibilityRole='link'
-        onPress={this._handlePress}
-        ref={this._setRef}
-        {...rest}
-      />
-    )
-  }
-}
+});
+
+export default App;
